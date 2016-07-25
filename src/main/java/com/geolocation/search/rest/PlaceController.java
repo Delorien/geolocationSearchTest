4138368a8879b1727ef12fdc6f66772e0c24df5d
Leonardo Tonin <leonardo.neto@payu.com.br>
2016-07-25 10:52:05 -0300

    Add Geolocation Service.

--- src/main/java/com/geolocation/search/rest/PlaceController.java
+++ src/main/java/com/geolocation/search/rest/PlaceController.java
@@ -3,10 +3,10 @@ package com.geolocation.search.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.geolocation.search.business.PlaceBusiness;
@@ -28,19 +28,23 @@ public class PlaceController {
 		return business.add(places);
 	}
 
-	@RequestMapping(value = "/place", method = RequestMethod.PUT)
-	public APIMessage update(@RequestParam(value = "name") String name, @RequestBody Place place) {
+	@RequestMapping(value = "/place/{name}", method = RequestMethod.PUT)
+	public APIMessage update(@PathVariable("name") String name, @RequestBody Place place) {
 		return business.update(name, place);
 	}
 
-	@RequestMapping(value = "/place", method = RequestMethod.GET)
-	public Place get(@RequestParam(value = "name") String name) {
+	@RequestMapping(value = "/place/{name}", method = RequestMethod.GET)
+	public Place get(@PathVariable("name") String name) {
 		return business.get(name);
 	}
 
 	@RequestMapping(value = "/places", method = RequestMethod.GET)
 	public List<Place> listAll() {
-		List<Place> places = business.listAll();
-		return places;
+		return business.listAll();
+	}
+
+	@RequestMapping(value = "/places/{name}/raio/{raio}", method = RequestMethod.GET)
+	public List<Place> listByLocationNear(@PathVariable("name") String name, @PathVariable("raio") Double raio) {
+		return business.listByLocationNear(name, raio);
 	}
 }
