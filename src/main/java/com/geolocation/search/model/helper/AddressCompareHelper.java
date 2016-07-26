4601658c3621a93cd6dc44eddd31a6e1ea1d21b3
Leonardo Tonin <leonardo.neto@payu.com.br>
2016-07-26 11:11:21 -0300

    Pass Jaro Winkler threshold to properties.

--- src/main/java/com/geolocation/search/model/helper/AddressCompareHelper.java
+++ src/main/java/com/geolocation/search/model/helper/AddressCompareHelper.java
@@ -1,12 +1,15 @@
 package com.geolocation.search.model.helper;
 
 import com.geolocation.search.model.Address;
+import com.geolocation.search.util.PropertiesLoader;
 
 import info.debatty.java.stringsimilarity.JaroWinkler;
 
 public class AddressCompareHelper {
 
-	private static final double JARO_WINKLER_ADDRESS_SIMILARITY_THRESHOLD = 0.75;
+	private static final double JARO_WINKLER_ADDRESS_SIMILARITY_THRESHOLD = Double
+			.parseDouble(PropertiesLoader.get("jaro.winkler.address.similarity.threshold"));
+
 	private static JaroWinkler jaroWinkler = new JaroWinkler();;
 
 	public static boolean isSameAddress(Address address, Address address2) {
--- src/main/java/com/geolocation/search/model/helper/PlaceCompareHelper.java
+++ src/main/java/com/geolocation/search/model/helper/PlaceCompareHelper.java
@@ -1,12 +1,15 @@
 package com.geolocation.search.model.helper;
 
 import com.geolocation.search.model.Place;
+import com.geolocation.search.util.PropertiesLoader;
 
 import info.debatty.java.stringsimilarity.JaroWinkler;
 
 public class PlaceCompareHelper {
 
-	private static final double JARO_WINKLER_SIMILARITY_THRESHOLD = 0.85;
+	private static final double JARO_WINKLER_SIMILARITY_THRESHOLD = Double
+			.parseDouble(PropertiesLoader.get("jaro.winkler.similarity.threshold"));
+
 	private static JaroWinkler jaroWinkler = new JaroWinkler();;
 
 	public static boolean isSameName(Place place1, Place place2) {
